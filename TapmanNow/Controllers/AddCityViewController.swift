02282859f2d14c0556daf9ea9a120f0c2759797e
Jignesh Patil <patiljignesh@gmail.com>
2023-11-19 23:59:24 +0200

    Base flow of the app add.

---
+++ TapmanNow/Controllers/AddCityViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AddCityViewController.swift
+//  TapmanNow
+//
+//  Created by Jigneshkumar Patil on 2023/11/19.
+//
+
+import Foundation
---
+++ TapmanNow/Controllers/HomeCityListTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  TapmanNow
+//
+//  Created by Jigneshkumar Patil on 2023/11/19.
+//
+
+import UIKit
+
+
+class HomeCityListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup(){
+        // Check for current location permission
+        // Based on the permission
+        //  - Ask for permission
+        //  - retrive current location coordinates
+        // Make web service call using current location
+        //
+    }
+}
